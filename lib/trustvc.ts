10cb9a7f390a83fcb77eacde34ade0a61103b82e
Laurence <26592081+lanlan3322@users.noreply.github.com>
2026-07-28 16:01:09 +0800

    test

--- lib/trustvc.ts
+++ lib/trustvc.ts
@@ -62,7 +62,7 @@ export function buildVCPayload(data: CertificateData) {
       "https://www.w3.org/ns/credentials/v2",
       "https://w3id.org/security/data-integrity/v2",
     ],
-    type: ["VerifiableCredential", "OpenCertsCertificate"],
+    type: ["VerifiableCredential"],
     credentialSubject: {
       type: ["Person"],
       name: data.recipientName,
