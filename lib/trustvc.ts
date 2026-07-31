fa2985e3046e524afe1a7c44793c41327a776c49
Laurence <26592081+lanlan3322@users.noreply.github.com>
2026-07-31 12:47:49 +0800

    test

--- lib/trustvc.ts
+++ lib/trustvc.ts
@@ -64,7 +64,6 @@ export function buildVCPayload(data: CertificateData) {
     "@context": [
       "https://www.w3.org/ns/credentials/v2",
       "https://w3id.org/security/data-integrity/v2",
-      "https://www.opencerts.io/schema/context/opencerts_v2.json"
     ],
     type: ["VerifiableCredential", "OpenAttestationCredential"],
     issuanceDate: data.issueDate,
